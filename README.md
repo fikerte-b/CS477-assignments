bc6b0494b8ee7d3ee5cedcf0e1508cb237de77c9
fikerte-b <fikirbenti@gmail.com>
2022-07-24 22:47:33 -0500

    lab2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CS477-assignments
 1. Lab0(https://github.com/fikerte-b/CS477-assignments/blob/main/lab0.js)
 2. Lab1 [https://github.com/fikerte-b/CS477-assignments/tree/main/lab1]
+3. Lab2 [https://github.com/fikerte-b/CS477-assignments/tree/main/lab2]
--- lab1/exercise1&2.js
+++ lab1/exercise1&2.js
@@ -17,16 +17,22 @@ arr.prototype.odd = function(){
  * setTimeout schedules a callback to run after a delayed time and
  * it is excuted in the timers phase of the event loop
  *  setImmediate runs at the check phase of the event loop and 
- * it sometimes it runs faster than setTimeout because it doesn't check timer
+ * it sometimes runs faster than setTimeout because it doesn't check timer
+ * If we want to do callbacks after  file I/O, using setImmediate is faster since it is excuted after I/O event in the exsiting cycle
+ * But to excute setTimeout in this case, the cycle has to be done and another new cycle has to start.
+ * 
  */
 
 /**question2
- * process.nextTick is not a part of the event loop while setrImmediate is a part of it.
+ * process.nextTick is not a part of the event loop while setImmediate is a part of it.
  * Node processes callbacks in the nextTick queue before processing the event loop callbacks.
+ * setImmediate has limited number of callback excutions but nextTick doesn't have a limit
+ * setImmediate has one chance of excution in every cycle while nexttick has multiple time since it checks...
+ * 
  * 
  */
 
 /**question3
  * setTimeout(), setInterval(), clearTimeout(),console.log(),exports,
- *  module,__dirname, require(), setImmediate()
+ *  module,__dirname, require(), setImmediate(), buffer
  */
